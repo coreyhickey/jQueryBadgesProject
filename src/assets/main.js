f368e904825aecc01e29dd9ae2ded12de78ded1e
coreyhickey <coreyhickey@gmail.com>
2017-08-18 11:46:11 -0400

    Finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  // your code will go here
+	$.ajax({
+		url: 'https://www.codeschool.com/users/coreyhickey.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			$.each(response.courses.completed, function (index, value) {	
+				$('#badges').append('<div class="course"><h3>' + this.title + '</h3><img src="' + this.badge + '" alt="' + this.title + '" ><a href="'+this.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+			});
+		}
+	});
 
 });
